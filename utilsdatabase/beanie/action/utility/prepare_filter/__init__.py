4173f69492faa28e4beaa7cf9b067fb8781bd5f5
Reza (Sam) Aghamohammadi <re.agha.mo@gmail.com>
2024-06-03 18:27:50 +0330

    20240603

--- utilsdatabase/beanie/action/utility/prepare_filter/__init__.py
+++ utilsdatabase/beanie/action/utility/prepare_filter/__init__.py
@@ -5,4 +5,4 @@ from .prepare_filter_for_regex_fields import prepare_filter_for_regex_fields
 # from .prepare_filter_for_has_value_fields import prepare_filter_for_boolean_fields
 from .prepare_filter_for_datetime_fields import prepare_filter_for_datetime_fields
 from .prepare_filter_for_in_fields import prepare_filter_for_in_fields
-from .prepare_filter_for_search_field import prepare_filter_for_search_field\
+from .prepare_filter_for_search_field import prepare_filter_for_search_field
